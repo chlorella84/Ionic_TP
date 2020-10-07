99b45c68ca86466707702a05174efd9a159e9b24
unknown <Administrateur@42SE34-4G0455J.ad.campus-eni.fr>
2020-10-07 11:32:11 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public pseudo : string = "";
+  public difficulty : string = "Easy" ;
+  public error : string = "";
+  public isVisibleForm : boolean = true;
+  public nextQuestion : boolean = false;
+  constructor(private alertCtrl : AlertController) {}
 
+  ngOnInit() {}
+
+  public async StartGame(){
+    console.log("CALL startGame");
+    this.error = "";
+
+    if(!this.pseudo || this.pseudo.length <= 3) { 
+      this.error = 'Your pseudo is too short (3 caracters minimum)'; 
+      const alert = await this.alertCtrl.create({ 
+        header : 'Pseudo', 
+        message:'Your pseudo is too short (3 caracters minimum)' 
+      }); 
+      alert.present();
+      return; 
+    } 
+    if(this.difficulty === undefined) { 
+      this.error = 'Please, choose the difficulty'; 
+      const alert = await this.alertCtrl.create({ 
+        header : 'Difficulty', 
+        message:'Please, choose the difficulty'
+      }); 
+      alert.present();
+      return; 
+    } 
+    return this.isVisibleForm = false;
+  }
+
+  private reponse(answer){
+  
+    return this.nextQuestion = true;
+  } 
 }
